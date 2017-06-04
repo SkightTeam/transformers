579abd9e0366877961134c4906ccc3b6a9ff12a2
System <none>
2017-06-04 18:17:43 +0000

    🚒🎙 Updated with Glitch

--- src/App.test.js
+++ src/App.test.js
@@ -12,30 +12,30 @@ it('renders without crashing', () => {
 it('Should correctly set bossVsBoss flag to true when Optimus and Predaking fight', () => {
     const autobots = [{
         'type': 'Autobot',
-        'strength': 8,
-        'intelligence': 1,
-        'speed': 8,
-        'endurance': 10,
-        'rank': 10,
-        'courage': 6,
+        'strength': 0,
+        'intelligence': 0,
+        'speed': 0,
+        'endurance': 0,
+        'rank': 0,
+        'courage': 0,
         'firepower': 0,
-        'skill': 4,
+        'skill': 0,
         'name': 'Optimus Prime',
-        'overallrating': 27
+        'overallrating': 0
     }];
 
     const decepticons = [{
         'type': 'Decepticon',
-        'strength': 8,
-        'intelligence': 1,
-        'speed': 8,
-        'endurance': 10,
-        'rank': 10,
-        'courage': 6,
+        'strength': 0,
+        'intelligence': 0,
+        'speed': 0,
+        'endurance': 0,
+        'rank': 0,
+        'courage': 0,
         'firepower': 0,
-        'skill': 4,
+        'skill': 0,
         'name': 'Predaking',
-        'overallrating': 27
+        'overallrating': 0
     }];
 
     const battleResult = _battle(decepticons, autobots);
@@ -46,33 +46,33 @@ it('Should correctly set bossVsBoss flag to true when Optimus and Predaking figh
 
 
 
-it('Should cause decepticon to flee (Autobot courage is >= 4 and Strength is >= 3)', () => {
+it('Should cause decepticon to flee based on  courage and strength', () => {
     const autobots = [{
         'type': 'Autobot',
         'strength': 8,
-        'intelligence': 1,
-        'speed': 8,
+        'intelligence': 0,
+        'speed': 0,
         'endurance': 10,
-        'rank': 10,
-        'courage': 10,
+        'rank': 0,
+        'courage': 7,
         'firepower': 0,
-        'skill': 4,
+        'skill': 0,
         'name': 'Landwhip',
-        'overallrating': 2
+        'overallrating': 0
     }];
 
     const decepticons = [{
         'type': 'Decepticon',
         'strength': 2,
-        'intelligence': 1,
-        'speed': 8,
+        'intelligence': 0,
+        'speed': 0,
         'endurance': 10,
-        'rank': 10,
+        'rank': 0,
         'courage': 3,
         'firepower': 0,
-        'skill': 4,
+        'skill': 0,
         'name': 'Rumblebeast',
-        'overallrating': 27
+        'overallrating': 0
     }];
 
     const battleResult = _battle(decepticons, autobots);
@@ -86,28 +86,28 @@ it('Should cause decepticon to flee (Autobot courage is >= 4 and Strength is >= 
 it('Should cause autobot to flee (Decepticon Overall Rating is higher)', () => {
     const autobots = [{
         'type': 'Autobot',
-        'strength': 8,
-        'intelligence': 1,
-        'speed': 8,
-        'endurance': 10,
-        'rank': 10,
-        'courage': 10,
+        'strength': 0,
+        'intelligence': 0,
+        'speed': 0,
+        'endurance': 0,
+        'rank': 0,
+        'courage': 0,
         'firepower': 0,
-        'skill': 4,
+        'skill': 0,
         'name': 'Landwhip',
         'overallrating': 2
     }];
 
     const decepticons = [{
         'type': 'Decepticon',
-        'strength': 7,
-        'intelligence': 1,
-        'speed': 8,
-        'endurance': 10,
-        'rank': 10,
-        'courage': 9,
+        'strength': 0,
+        'intelligence': 0,
+        'speed': 0,
+        'endurance': 0,
+        'rank': 0,
+        'courage': 0,
         'firepower': 0,
-        'skill': 4,
+        'skill': 0,
         'name': 'Rumblebeast',
         'overallrating': 27
     }];
